4094ab3ef18e16df841449a0f870e99ff79b1db2
katherine <pv.katherine7@gmail.com>
2023-02-06 20:26:13 +0000

    new entry button styles

--- journal-app/src/pages/TimeLine/TimeLine.jsx
+++ journal-app/src/pages/TimeLine/TimeLine.jsx
@@ -1,9 +1,11 @@
 import TimeLineEntry from "../../components/TimeLineEntry/TimeLineEntry";
 import "./TimeLine.scss";
 import Layout from "../../components/Layout/Layout";
+import Button from "../../components/Button/Button";
 
 const TimeLine = () => {
   const title = "User's Journal";
+  const buttonText = "+ New Entry";
   return (
     <Layout title={title}>
       <div className="timeline">
@@ -12,6 +14,7 @@ const TimeLine = () => {
         <TimeLineEntry />
         <TimeLineEntry />
         <TimeLineEntry />
+        <Button buttonText={buttonText} />
       </div>
     </Layout>
   );
