45d1c762b3ada14c598907ffc1295fc0ac7922e3
katherine <pv.katherine7@gmail.com>
2023-02-06 19:57:49 +0000

    styles for layout and timeline

--- journal-app/src/App.jsx
+++ journal-app/src/App.jsx
@@ -1,16 +1,18 @@
-import './App.scss';
-import { Route, Routes } from 'react-router-dom';
-import TimeLine from './pages/TimeLine/TimeLine';
-import Quote from './pages/Quote/Quote';
-import NewEntry from './pages/NewEntry/NewEntry';
+import "./App.scss";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import TimeLine from "./pages/TimeLine/TimeLine";
+import Quote from "./pages/Quote/Quote";
+import NewEntry from "./pages/NewEntry/NewEntry";
 const App = () => {
   return (
-    <Routes>
-      <Route path="*" element={<TimeLine/>}/>
-      <Route path="/new" element={<NewEntry/>}/>
-      <Route path="/quote" element={<Quote/>} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="*" element={<TimeLine />} />
+        <Route path="/new" element={<NewEntry />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
