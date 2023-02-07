69ae67cde7210e816baa418350f5de70cb2ca395
unknown <pv.katherine7@gmail.com>
2023-02-07 16:17:55 +0000

    added to EntriesController and EntriesService

--- journal/src/main/java/com/example/journal/EntriesRepository.java
+++ journal/src/main/java/com/example/journal/EntriesRepository.java
@@ -1,4 +1,10 @@
 package com.example.journal;
 
-public interface EntriesRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EntriesRepository extends JpaRepository<Entry,Long> {
+    // write custom methods here
+    void deleteEntryById(long id);
 }
--- journal/src/main/java/com/example/journal/EntriesService.java
+++ journal/src/main/java/com/example/journal/EntriesService.java
@@ -1,4 +1,51 @@
 package com.example.journal;
 
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.*;
+
+@Service
 public class EntriesService {
+
+    @Autowired
+    EntriesRepository entriesRepository;
+
+    // CREATE
+    public void addEntry(Entry entry) {
+        entriesRepository.save(entry);
+    }
+
+    // READ
+    public Entry getEntryById(long id) {
+        Optional<Entry> entry = entriesRepository.findById(id);
+        if (entry.isEmpty()) {
+            throw new EntryNotFoundException();
+        }
+        return entry.get();
+    }
+
+    public List<Entry> getAllEntries() {
+        return entriesRepository.findAll();
+    }
+
+    // UPDATE
+    public void updateEntry(Entry newEntry, long id) {
+        if(!entriesRepository.existsById(id)) {
+            throw new EntryNotFoundException();
+        }
+        newEntry.setId(id);
+        entriesRepository.save(newEntry);
+    }
+
+    // DELETE
+    @Transactional
+    public void deleteEntryById(long id) {
+        if (!entriesRepository.existsById(id)) {
+            throw new EntryNotFoundException();
+        }
+        entriesRepository.deleteEntryById(id);
+    }
 }
