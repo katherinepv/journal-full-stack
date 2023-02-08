2c16b3e0117c68fe8403aa4e52ec6389dd9c4f48
katherine <pv.katherine7@gmail.com>
2023-02-08 12:17:42 +0000

    added edit entry page and route path for it

--- journal-app/src/App.jsx
+++ journal-app/src/App.jsx
@@ -3,12 +3,15 @@ import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import TimeLine from "./pages/TimeLine/TimeLine";
 import Quote from "./pages/Quote/Quote";
 import CreateEntry from "./pages/CreateEntry/CreateEntry";
+import EdityEntry from "./pages/EditEntry/EditEntry";
+
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="*" element={<TimeLine />} />
         <Route path="/new" element={<CreateEntry />} />
+        <Route path="/edit/:id" element={<EdityEntry />} />
         <Route path="/quote" element={<Quote />} />
       </Routes>
     </Router>
