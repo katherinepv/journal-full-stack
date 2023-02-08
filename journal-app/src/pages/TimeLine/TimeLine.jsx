f7db439d3fc6dea412a3a482f00e6b5f053e7e08
katherine <pv.katherine7@gmail.com>
2023-02-08 12:07:26 +0000

    added state and connected to api with fetch request for all entries to be displayed in timeline

--- journal-app/src/pages/TimeLine/TimeLine.jsx
+++ journal-app/src/pages/TimeLine/TimeLine.jsx
@@ -4,14 +4,27 @@ import Layout from "../../components/Layout/Layout";
 import Button from "../../components/Button/Button";
 import { Link } from "react-router-dom";
 import EntriesList from "../../components/EntriesList/EntriesList";
+import { useEffect } from "react";
 
 const TimeLine = () => {
+  const [entries, setEntries] = useState([]);
   const title = "User's Journal";
   const buttonText = "+ New Entry";
+
+  const getEntries = async () => {
+    const response = await fetch("http://localhost:8080/entries");
+    const entriesData = await response.json();
+    setEntries(entriesData);
+  };
+
+  useEffect(() => {
+    getEntries();
+  }, []);
+
   return (
     <Layout title={title}>
       <div className="timeline">
-        <EntriesList />
+        <EntriesList entries={entries} />
         <Link to="/new">
           <Button buttonText={buttonText} />
         </Link>
